498058fad3347b69e3346fe332126ee787c5e27e
shreyaspatel21 <90972236+shreyaspatel21@users.noreply.github.com>
2022-12-23 12:39:46 +0000

    Add project files.

---
+++ FizzBuzzTestConsoleApp/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTestConsoleApp.FizzBuzz
+{
+    public class FizzBuzz : IFizzBuzz
+    {
+        IFizzBuzzOutput __FizzBuzzOutput;
+        private const string FIZZ_STRING = "FIZZ";
+        private const string BUZZ_STRING = "BUZZ";
+
+        public int __StartFrom {
+            get
+            {
+                return __StartFrom;
+            }
+            set
+            {
+                __StartFrom = value;
+            }
+        }
+        public int __StopAt
+        {
+            get
+            {
+                return __StopAt;
+            }
+            set
+            {
+                __StopAt= value;
+            }
+        }
+
+        public FizzBuzz() 
+            : this(new FizzBuzzOutput())
+        {
+        }
+
+        private FizzBuzz(IFizzBuzzOutput fizzBuzzOutput)
+        {
+            __FizzBuzzOutput = fizzBuzzOutput;
+        }
+
+
+
+
+    }
+}
---
+++ FizzBuzzTestConsoleApp/FizzBuzz/FizzBuzzOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTestConsoleApp.FizzBuzz
+{
+    internal class FizzBuzzOutput : IFizzBuzzOutput
+    {
+        public void OutputNextElement(string nextElement) 
+        {
+            Console.WriteLine(nextElement);
+        }
+    }
+}
